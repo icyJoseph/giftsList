a4b4eabfccc9888e5692f1f87a5a7e79c3501d87
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 12:33:56 +0100

    test: type person name changes the state correctly

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -12,4 +12,18 @@ describe("Gift", () => {
   it("initializes a person and present in `state`", () => {
     expect(gift.state()).toEqual({ person: "", present: "" });
   });
+
+  describe("when typing into the person input", () => {
+    const person = "Uncle";
+
+    beforeEach(() => {
+      gift
+        .find(".input-person")
+        .simulate("change", { target: { value: person } });
+    });
+
+    it("updates the person in state", () => {
+      expect(gift.state().person).toEqual(person);
+    });
+  });
 });
