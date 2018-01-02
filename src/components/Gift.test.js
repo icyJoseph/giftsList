18a45be49436815693f57d8a638ca525776529b8
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 12:26:40 +0100

    test: verify state has the correct structure

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -4,7 +4,12 @@ import Gift from "./Gift";
 
 describe("Gift", () => {
   const gift = shallow(<Gift />);
+
   it("renders properly", () => {
     expect(gift).toMatchSnapshot();
+  });
+
+  it("initializes a person and present in `state`", () => {
+    expect(gift.state()).toEqual({ person: "", present: "" });
   });
 });
