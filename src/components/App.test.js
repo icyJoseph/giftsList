6f10fae1edb3d45f40f67bb5d0026c6ad04e0456
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 11:05:51 +0100

    test: new test for added gift with id:1

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,3 +11,11 @@ it("renders correctly", () => {
 it("initializes the `state` with an empty lists of gifts", () => {
   expect(app.state().gifts).toEqual([]);
 });
+
+it("adds a new gift to `state` the `add gift` button", () => {
+  // Find the `add gift` button by className (.className) of jsx tag
+  // and then simulate a click
+  app.find(".btn-add").simulate("click");
+  // Once you add a gift the state.gifts is not []
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
