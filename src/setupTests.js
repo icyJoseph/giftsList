0834a9205debd933fc5fee2e76f01cd3e180ec93
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 10:42:39 +0100

    fix: typo in adapter declaration

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,4 @@ import requestAnimationFrame from "./tempPolyfills";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-configure({ adpter: new Adapter(), disableLifecycleMethods: true });
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
