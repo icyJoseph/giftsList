6fc7a0d66e151648b776896edaf0a94c87f77104
joseph_ctu <sephxd1234@gmail.com>
2018-01-02 10:49:07 +0100

    feat: added new heading

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Gifts</h2>
+      </div>
+    );
   }
 }
 
